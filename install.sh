c3da5c2b39bf26f49a8404daed2567653d4c4d89
Gary Moore <madmangaz@gmail.com>
2021-09-20 20:05:38 +0100

    extract saving password to own function

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 # Save chosen password to file. This is so we can read it
 # when creating the container, and when connecting to the
 # container.
-read -sp "Choose a password: " passvar
-echo $passvar > pass.txt
+function createPassword() {
+    read -rsp "Choose a password: " passvar
+    echo "$passvar" > pass.txt
+}
 
 echo "Creating MS SQL Install."
+createPassword
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$(cat pass.txt)" -e "MSSQL_PID=Express" -p 1433:1433 --name mssql -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
 echo "Created and Started MS SQL Server with Password: '$(cat pass.txt)'."
