c6c8d9acd505bbc19b3200ead15d789d654b4b96
Gary Moore <madmangaz@gmail.com>
2021-09-20 20:15:38 +0100

    move container creation into it's own function

--- install.sh
+++ install.sh
@@ -8,7 +8,21 @@ function createPassword() {
     echo "$passvar" > pass.txt
 }
 
+# Build the MS SQL container with bog standard defaults.
+# Takes a password as a parameter.
+function buildContainer() {
+    docker run \
+    -e "ACCEPT_EULA=Y" \
+    -e "SA_PASSWORD=$1" -e \
+    "MSSQL_PID=Express" \
+    -p 1433:1433 \
+    --name mssql \
+    -d \
+    mcr.microsoft.com/mssql/server:2017-latest-ubuntu \
+    >/dev/null
+}
+
 echo "Creating MS SQL Install."
 createPassword
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$(cat pass.txt)" -e "MSSQL_PID=Express" -p 1433:1433 --name mssql -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+buildContainer "$(cat pass.txt)"
 echo "Created and Started MS SQL Server with Password: '$(cat pass.txt)'."
